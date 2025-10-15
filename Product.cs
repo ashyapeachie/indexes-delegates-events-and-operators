0499918aad7e81b1e679dcc031b5b225bf85d21b
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-10-14 22:05:06 -0400

    descriptions

--- Product.cs
+++ Product.cs
@@ -1,5 +1,6 @@
 /* 
 Author:
 Filename:
-Description:
+Description: class that represents a product with a code and description and supports
+    equality comparisons using == and != operators
  */
--- ProductList.cs
+++ ProductList.cs
@@ -1,5 +1,7 @@
 /* 
 Author:
 Filename:
-Description:
+Description: class wraps a List<Product>; supports access via indexer (int or string),
+    custom + and - operators to add / remove products, and a delegate + event system
+    that fires when products are changed
  */
