29f3df734284cbf957247a60bcfacfe8007f16f5
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-10-29 13:55:26 -0400

    adding code to Program.cs
    the code I just added is already in the file according to my chosen editing software: Visual Studio Code. I'm not sure why GitHub desktop is not saving my changes but I was able to make changes when I was at home (currently on campus)

--- Program.cs
+++ Program.cs
@@ -3,3 +3,25 @@ Author: Aham
 Filename: Programs.cs
 Description: 
  */
+
+using System;
+
+class Program {
+    static void Main() {
+        var list = new ProductList();
+
+        list.Changes += () => Console.WriteLine("Product list has chaged!");
+        // event using a lambda
+
+        var p1 = new Product();
+        var p2 = new Product();
+        var p3 = new Product();
+        // creating products
+
+        // add & remove elements using operator overloads
+
+        Console.WriteLine("First product:");
+        Console.WriteLine(list[0]);
+        // access via indexer
+    }
+}
